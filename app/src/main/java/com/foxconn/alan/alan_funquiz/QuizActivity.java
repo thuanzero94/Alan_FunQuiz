f3e1f1fcebd21773a74fa2cab71ef20f02e8fe7e
lanzero <thuanzero94@gmail.com>
2018-02-26 16:31:42 +0700

    Create Button object and Event onclick

--- app/src/main/java/com/foxconn/alan/alan_funquiz/QuizActivity.java
+++ app/src/main/java/com/foxconn/alan/alan_funquiz/QuizActivity.java
@@ -2,12 +2,34 @@ package com.foxconn.alan.alan_funquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class QuizActivity extends AppCompatActivity {
+    private static final String TAG = QuizActivity.class.getSimpleName();
+
+    private Button mBtnTrue;
+    private Button mBtnFalse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        mBtnTrue = (Button) findViewById(R.id.Btn_true);
+        mBtnTrue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Do nothings
+            }
+        });
+
+        mBtnFalse = (Button) findViewById(R.id.Btn_false);
+        mBtnFalse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Do nothings
+            }
+        });
     }
 }
