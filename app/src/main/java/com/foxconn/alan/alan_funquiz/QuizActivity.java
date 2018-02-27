601ec7237bb1c65c13e2ea6ef083237bedbfb8d5
lanzero <thuanzero94@gmail.com>
2018-02-27 13:21:39 +0700

    Code design follow MVC template \n -Model object: Question.java \n -View object: activity_quiz.xml \n -Controller object: QuizActivity.java \n #Add Next Button #Add Event click Button, check answer and next question \n Author: Alan

--- app/src/main/java/com/foxconn/alan/alan_funquiz/QuizActivity.java
+++ app/src/main/java/com/foxconn/alan/alan_funquiz/QuizActivity.java
@@ -2,14 +2,29 @@ package com.foxconn.alan.alan_funquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
     private static final String TAG = QuizActivity.class.getSimpleName();
 
     private Button mBtnTrue;
     private Button mBtnFalse;
+    private Button mBtnNext;
+    private TextView mQuestTextView;
+
+    private Question[] mQuestionBank = new Question[] {
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_oceans, true),
+    };
+
+    private int mCurrentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,19 +32,58 @@ public class QuizActivity extends AppCompatActivity {
         setContentView(R.layout.activity_quiz);
 
         mBtnTrue = (Button) findViewById(R.id.Btn_true);
+        mBtnFalse = (Button) findViewById(R.id.Btn_false);
+        mBtnNext = (Button) findViewById(R.id.Btn_Next);
+
+        mQuestTextView = (TextView) findViewById(R.id.text_question);
+
+        setButtonOnClick();
+        updateQuestion();
+    }
+
+    private void alanMakeToast(int text){
+        Log.i(TAG, "alanMakeToast");
+        Toast.makeText(QuizActivity.this, text, Toast.LENGTH_SHORT).show();
+    }
+
+    private void setButtonOnClick(){
         mBtnTrue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Do nothings
+                checkAnswer(true);
             }
         });
 
-        mBtnFalse = (Button) findViewById(R.id.Btn_false);
         mBtnFalse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Do nothings
+                checkAnswer(false);
+            }
+        });
+
+        mBtnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
             }
         });
     }
+
+    private void updateQuestion(){
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestTextView.setText(question);
+    }
+
+    private void checkAnswer(boolean userPressedTrue){
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+
+        if(userPressedTrue == answerIsTrue){
+            alanMakeToast(R.string.correct_toast);
+        } else {
+            alanMakeToast(R.string.incorrect_toast);
+        }
+
+    }
+
 }
